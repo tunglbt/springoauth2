6b828f64c775d7eb3e6b536a1e7050107beddddd
Tung <lebuithanhtung@gmail.com>
2021-05-08 22:25:52 +0700

    add spring boot

---
+++ src/test/java/com/tunglbt/springoauth2/Springoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunglbt.springoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
