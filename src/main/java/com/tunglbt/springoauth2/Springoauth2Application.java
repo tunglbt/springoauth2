32746ed6b7582592352c5699f2dab200c69a3144
Tung <lebuithanhtung@gmail.com>
2021-05-14 01:15:45 +0700

    Revert "Add configuration files"
    This reverts commit a7b9c512

--- src/main/java/com/tunglbt/springoauth2/Springoauth2Application.java
+++ src/main/java/com/tunglbt/springoauth2/Springoauth2Application.java
@@ -2,12 +2,8 @@ package com.tunglbt.springoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
 public class Springoauth2Application {
 
 	public static void main(String[] args) {
